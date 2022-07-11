19c1f50a3e4f67eb656f473f122dff598d53c741
smiling-mask <smilingmask.sana@gmail.com>
2022-07-11 15:09:37 +0900

    import/order eslint 옵션 적용

---
+++ src/_pages/App/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import { AppContainer } from './styles';
+
+const App: FC = () => {
+  return <AppContainer></AppContainer>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-import './index.css';
-import App from '_pages/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+import App from '_pages/App';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
